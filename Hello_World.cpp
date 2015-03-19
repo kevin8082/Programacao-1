a7ea026a1dbc6875725503f306b3d9b87976b47c
kevin8082 <kevinho8866@hotmail.com>
2015-03-19 08:30:31 -0300

    Create Hello_World.cpp

---
+++ Hello_World.cpp
@@ -0,0 +1,12 @@
+// Como funciona pra na tela você poder mostrar o Hello World e pode ve ele sem que feche a tela usando o Pause
+// Feito Por Kevin8082
+
+#include <iostream>
+using namespace std;
+
+void main(void){
+
+	cout << "Hello World" << endl;
+
+	system("pause");
+}
