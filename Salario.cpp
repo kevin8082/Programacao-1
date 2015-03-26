713a5556bdf9a06a4e6235c818ea924f674ada38
kevin8082 <kevinho8866@hotmail.com>
2015-03-25 22:23:19 -0300

    Unicode eu te amo devolta

--- Salario.cpp
+++ Salario.cpp
@@ -2,11 +2,17 @@
 // o valor a mais de hora extra com um multiplicador de valor para horas extras com uma leve modificação de
 // poder colocar quantas horas a trabalhar no total 
 // Feito Por Kevin8082
+// cout= escrever texto na tela
+// cin= salvar a informação
+// double= variavel que aceita números decimais
 
 #include <iostream>
+#include <locale>;
 using namespace std;
 
 void main(void){
+
+	setlocale(LC_ALL, "");
 
 	double Total_Horas;
 	double Horas_Trabalhadas;
@@ -19,7 +25,7 @@ void main(void){
 	double Salario;
 
 	// Ler Total_Horas	
-	cout << "Digite o Total de Horas a Trabalhar:";
+	cout << "Digite o Total de Horas à Trabalhar:";
 	cin >> Total_Horas;
 
 	// Ler Horas_Trabalhadas	
@@ -27,11 +33,11 @@ void main(void){
 	cin >> Horas_Trabalhadas;
 
 	// Ler Salario_Hora	
-	cout << "Digite o Salario Hora:";
+	cout << "Digite o Salário por Hora:";
 	cin >> Salario_Hora;
 
 	// Ler Multiplicador_Hora_Extra	
-	cout << "Multiplicador da Hora Extra(use ponto nao virgula):";
+	cout << "Multiplicador da Hora Extra(use ponto não vírgula):";
 	cin >> Multiplicador_Hora_Extra;
 
 	// Hora_Extra	
@@ -56,11 +62,11 @@ void main(void){
 
 	// Imprimir
 	if (Horas_Trabalhadas > Total_Horas){
-		cout << "Seu Salario e: " << Salario << endl;
+		cout << "Seu Salário é: " << Salario << endl;
 		cout << "Horas Extras Trabalhadas: " << Hora_Extra << endl;
 	}
 	else{
-		cout << "Seu Salario e: " << Salario << endl;
+		cout << "Seu Salário é: " << Salario << endl;
 	}
 
 	system("pause");
