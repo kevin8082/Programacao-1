d38c179b8e076dd24b015f3bba39cfa6e389e6af
kevin8082 <kevinho8866@hotmail.com>
2015-04-09 20:56:55 -0300

    Create Temp.cpp

---
+++ Temp.cpp
@@ -0,0 +1,29 @@
+#include <iostream>;
+#include <locale>;
+using namespace std;
+
+void main(void){
+
+	setlocale(LC_ALL, "");
+
+	double Temp = 0.0;
+
+	cout << "Digite a temperatura em Kelvins:";
+	cin >> Temp;
+
+	if (Temp > 273 && Temp < 473){
+		cout << "A água está líquida." << endl;
+	}
+	else{
+		if (Temp <= 273){
+			cout << "A água está sólida." << endl;
+		}
+		else{
+			if (Temp >= 473)
+				cout << "A água está Gasosa." << endl;
+		}
+	}
+
+	system("pause");
+
+}
