d985fba4fa9660257d6725e0d89213551a483255
kevin8082 <kevinho8866@hotmail.com>
2015-04-09 19:04:46 -0300

    Create Bhaskara.cpp

---
+++ Bhaskara.cpp
@@ -0,0 +1,44 @@
+#include <iostream>;
+#include <locale>;
+#include <math.h>;
+using namespace std;
+
+void main(void){
+
+	setlocale(LC_ALL, "");
+
+	double a = 0.0;
+	double b = 0.0;
+	double c = 0.0;
+	double Delta = 0.0;
+	double Bhaskara1 = 0.0;
+	double Bhaskara2 = 0.0;
+
+	cout << "Digite A:";
+	cin >> a;
+
+	cout << "Digite B:";
+	cin >> b;
+
+	cout << "Digite C:";
+	cin >> c;
+
+	Delta = pow(b, 2) - 4 * a * c;
+
+	Bhaskara1 = ((-b) + sqrt(Delta)) / (2 * a);
+
+	Bhaskara2 = ((-b) - sqrt(Delta)) / (2 * a);
+
+	if (Delta > 0){
+		cout << "X1 é:" << Bhaskara1 << endl;
+		cout << "X2 é:" << Bhaskara2 << endl;
+	}
+	else{
+		if (Delta < 0){
+			cout << "A resposta é raiz imaginária e não será resolvido" << endl;
+		}
+	}
+
+	system("pause");
+
+}
