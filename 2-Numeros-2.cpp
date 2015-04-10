f6a75bad47aeb97d0304a5380d64f0fdc9b025cf
kevin8082 <kevinho8866@hotmail.com>
2015-04-10 10:12:04 -0300

    Create 2-Numeros-2.cpp

---
+++ 2-Numeros-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>;
+#include <locale>;
+using namespace std;
+
+void main(void){
+
+	setlocale(LC_ALL, "");
+
+	double num1 = 0.0;
+	double num2 = 0.0;
+	double Area_Retangulo = 0.0;
+	double Area_Triangulo_Retangulo = 0.0;
+	double Perimetro_Triangulo = 0.0;
+	double Perimetro_Retangulo = 0.0;
+
+	cout << "digite 2 números reais separados por um espaço:";
+	cin >> num1 >> num2;
+
+	Area_Retangulo = num1 * num2;
+
+	Area_Triangulo_Retangulo = (num1 * num2) / 2;
+
+	Perimetro_Triangulo = num1 + num2;
+
+	Perimetro_Retangulo = 2 * Area_Retangulo;
+
+	cout << "A área do retângulo é:" << Area_Retangulo << endl;
+	cout << "A área do triângulo retângulo é:" << Area_Triangulo_Retangulo << endl;
+	cout << "O perímetro do triângulo é:" << Perimetro_Triangulo << endl;
+	cout << "O perímetro do retângulo é:" << Perimetro_Retangulo << endl;
+
+	system("pause");
+
+}
