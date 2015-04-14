774175f0bfba0072317657304ca26f8259abf753
kevin8082 <kevinho8866@hotmail.com>
2015-04-14 18:00:03 -0300

    muda de bhascara pra x

--- Lista-2/Bhaskara.cpp
+++ Lista-2/Bhaskara.cpp
@@ -11,8 +11,8 @@ void main(void){
 	double b = 0.0;
 	double c = 0.0;
 	double Delta = 0.0;
-	double Bhaskara1 = 0.0;
-	double Bhaskara2 = 0.0;
+	double x1 = 0.0;
+	double x2 = 0.0;
 
 	cout << "Digite A:";
 	cin >> a;
@@ -25,13 +25,13 @@ void main(void){
 
 	Delta = pow(b, 2) - (4 * a * c);
 
-	Bhaskara1 = ((-b) + sqrt(Delta)) / (2 * a);
+	x1 = ((-b) + sqrt(Delta)) / (2 * a);
 
-	Bhaskara2 = ((-b) - sqrt(Delta)) / (2 * a);
+	x2 = ((-b) - sqrt(Delta)) / (2 * a);
 
 	if (Delta > 0){
-		cout << "X1 é:" << Bhaskara1 << endl;
-		cout << "X2 é:" << Bhaskara2 << endl;
+		cout << "X1 é:" << x1 << endl;
+		cout << "X2 é:" << x2 << endl;
 	}
 	else{
 		if (Delta < 0){
