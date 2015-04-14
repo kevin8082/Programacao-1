11d424be2eff4624bbf4c77f8fa374b48371ceaa
kevin8082 <kevinho8866@hotmail.com>
2015-04-14 14:58:49 -0300

    Arrumado o code, valeu N4rk

--- Lista-2/2-Numeros-2.cpp
+++ Lista-2/2-Numeros-2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>;
 #include <locale>;
+#include <math.h>;
 using namespace std;
 
 void main(void){
@@ -12,17 +13,20 @@ void main(void){
 	double Area_Triangulo_Retangulo = 0.0;
 	double Perimetro_Triangulo = 0.0;
 	double Perimetro_Retangulo = 0.0;
+	double Hipotenusa = 0.0;
 
 	cout << "digite 2 números reais separados por um espaço:";
 	cin >> num1 >> num2;
+
+	Hipotenusa = sqrt(pow(num1, 2)+pow(num2, 2));
 
 	Area_Retangulo = num1 * num2;
 
 	Area_Triangulo_Retangulo = (num1 * num2) / 2;
 
-	Perimetro_Triangulo = num1 + num2;
+	Perimetro_Triangulo = num1 + num2 + Hipotenusa;
 
-	Perimetro_Retangulo = 2 * Area_Retangulo;
+	Perimetro_Retangulo = 2 * (num1 + num2);
 
 	cout << "A área do retângulo é:" << Area_Retangulo << endl;
 	cout << "A área do triângulo retângulo é:" << Area_Triangulo_Retangulo << endl;
