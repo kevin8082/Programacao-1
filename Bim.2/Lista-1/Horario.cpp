82de9702b0ec173bb62223f17df379e6a86a51c3
kevin8082 <kevinho8866@hotmail.com>
2015-05-19 10:23:48 -0300

    Create Horario.cpp

---
+++ Bim.2/Lista-1/Horario.cpp
@@ -0,0 +1,468 @@
+#include <iostream>;
+#include <locale>;
+using namespace std;
+
+void main(void){
+
+	setlocale(LC_ALL, "");
+
+	int Horas = 0;
+	int Minutos = 0;
+	int Segundos = 0;
+
+	cout << "Digite as Horas, Minutos e Segundos respectivamente:";
+	cin >> Horas >> Minutos >> Segundos;
+
+	switch(Horas){
+	case 0:
+		cout << "Zero Horas, ";
+	break;
+	case 1:
+		cout << "Uma Hora, ";
+	break;
+	case 2:
+		cout << "Duas Horas, ";
+	break;
+	case 3:
+		cout << "Três Horas, ";
+	break;
+	case 4:
+		cout << "Quatro Horas, ";
+	break;
+	case 5:
+		cout << "Cinco Horas, ";
+	break;
+	case 6:
+		cout << "Seis Horas, ";
+	break;
+	case 7:
+		cout << "Sete Horas, ";
+	break;
+	case 8:
+		cout << "Oito Horas, ";
+	break;
+	case 9:
+		cout << "Nove Horas, ";
+	break;
+	case 10:
+		cout << "Dez Horas, ";
+	break;
+	case 11:
+		cout << "Onze Horas, ";
+	break;
+	case 12:
+		cout << "Doze Horas, ";
+	break;
+	case 13:
+		cout << "Treze Horas, ";
+	break;
+	case 14:
+		cout << "Quatorze Horas, ";
+	break;
+	case 15:
+		cout << "Quinze Horas, ";
+	break;
+	case 16:
+		cout << "Dezesseis Horas, ";
+	break;
+	case 17:
+		cout << "Dezessete Horas, ";
+	break;
+	case 18:
+		cout << "Dezoito Horas, ";
+	break;
+	case 19:
+		cout << "dezenove Horas, ";
+	break;
+	case 20:
+		cout << "Vinte Horas, ";
+	break;
+	case 21:
+		cout << "Vinte e Uma Horas, ";
+	break;
+	case 22:
+		cout << "Vinte e Duas Horas, ";
+	break;
+	case 23:
+		cout << "Vinte e Três Horas, ";
+	break;
+	case 24:
+		cout << "Vinte e Quatro Horas, ";
+	break;
+	}
+
+	switch(Minutos){
+	case 0:
+		cout << "Zero Minutos ";
+	break;
+	case 1:
+		cout << "Um Minuto ";
+	break;
+	case 2:
+		cout << "Dois Minutos ";
+	break;
+	case 3:
+		cout << "Três Minutos ";
+	break;
+	case 4:
+		cout << "Quatro Minutos ";
+	break;
+	case 5:
+		cout << "Cinco Minutos ";
+	break;
+	case 6:
+		cout << "Seis Minutos ";
+	break;
+	case 7:
+		cout << "Sete Minutos ";
+	break;
+	case 8:
+		cout << "Oito Minutos ";
+	break;
+	case 9:
+		cout << "Nove Minutos ";
+	break;
+	case 10:
+		cout << "Dez Minutos ";
+	break;
+	case 11:
+		cout << "Onze Minutos ";
+	break;
+	case 12:
+		cout << "Doze Minutos ";
+	break;
+	case 13:
+		cout << "Treze Minutos ";
+	break;
+	case 14:
+		cout << "Quatorze Minutos ";
+	break;
+	case 15:
+		cout << "Quinze Minutos ";
+	break;
+	case 16:
+		cout << "Dezesseis Minutos ";
+	break;
+	case 17:
+		cout << "Dezessete Minutos ";
+	break;
+	case 18:
+		cout << "Dezoito Minutos ";
+	break;
+	case 19:
+		cout << "Dezenove Minutos ";
+	break;
+	case 20:
+		cout << "Vinte Minutos ";
+	break;
+	case 21:
+		cout << "Vinte e Um Minutos ";
+	break;
+	case 22:
+		cout << "Vinte e Dois Minutos ";
+	break;
+	case 23:
+		cout << "Vinte e Três Minutos ";
+	break;
+	case 24:
+		cout << "Vinte e Quatro Minutos ";
+	break;
+	case 25:
+		cout << "Vinte e Cinco Minutos ";
+	break;
+	case 26:
+		cout << "Vinte e Seis Minutos ";
+	break;
+	case 27:
+		cout << "Vinte e Sete Minutos ";
+	break;
+	case 28:
+		cout << "Vinte e Oito Minutos ";
+	break;
+	case 29:
+		cout << "Vinte e Nove Minutos ";
+	break;
+	case 30:
+		cout << "Trinta Minutos ";
+	break;
+	case 31:
+		cout << "Trinta e Um Minutos ";
+	break;
+	case 32:
+		cout << "Trinta e Dois Minutos ";
+	break;
+	case 33:
+		cout << "Trinta e Três Minutos ";
+	break;
+	case 34:
+		cout << "Trinta e Quatro Minutos ";
+	break;
+	case 35:
+		cout << "Trinta e Cinco Minutos ";
+	break;
+	case 36:
+		cout << "Trinta e Seis Minutos ";
+	break;
+	case 37:
+		cout << "Trinta e Sete Minutos ";
+	break;
+	case 38:
+		cout << "Trinta e Oito Minutos ";
+	break;
+	case 39:
+		cout << "Trinta e Nove Minutos ";
+	break;
+	case 40:
+		cout << "Quarenta Minutos ";
+	break;
+	case 41:
+		cout << "Quarenta e Um Minutos ";
+	break;
+	case 42:
+		cout << "Quarenta e Dois Minutos ";
+	break;
+	case 43:
+		cout << "Quarenta e Três Minutos ";
+	break;
+	case 44:
+		cout << "Quarenta e Quatro Minutos ";
+	break;
+	case 45:
+		cout << "Quarenta e Cinco Minutos ";
+	break;
+	case 46:
+		cout << "Quarenta e Seis Minutos ";
+	break;
+	case 47:
+		cout << "Quarenta e Sete Minutos ";
+	break;
+	case 48:
+		cout << "Quarenta e Oito Minutos ";
+	break;
+	case 49:
+		cout << "Quarenta e Nove Minutos ";
+	break;
+	case 50:
+		cout << "Cinquenta Minutos ";
+	break;
+	case 51:
+		cout << "Cinquenta e Um Minutos ";
+	break;
+	case 52:
+		cout << "Cinquenta e Dois Minutos ";
+	break;
+	case 53:
+		cout << "Cinquenta e Três Minutos ";
+	break;
+	case 54:
+		cout << "Cinquenta e Quatro Minutos ";
+	break;
+	case 55:
+		cout << "Cinquenta e Cinco Minutos ";
+	break;
+	case 56:
+		cout << "Cinquenta e Seis Minutos ";
+	break;
+	case 57:
+		cout << "Cinquenta e Sete Minutos ";
+	break;
+	case 58:
+		cout << "Cinquenta e Oito Minutos ";
+	break;
+	case 59:
+		cout << "Cinquenta e Nove Minutos ";
+	break;
+	case 60:
+		cout << "Sessenta Minutos ";
+	break;
+	}
+
+	switch (Segundos){
+	case 0:
+		cout << "e Zero Segundos" << endl;
+	break;
+	case 1:
+		cout << "e Um Segundo" << endl;
+		break;
+	case 2:
+		cout << "e Dois Segundos" << endl;
+		break;
+	case 3:
+		cout << "e Três Segundos" << endl;
+		break;
+	case 4:
+		cout << "e Quatro Segundos" << endl;
+		break;
+	case 5:
+		cout << "e Cinco Segundos" << endl;
+		break;
+	case 6:
+		cout << "e Seis Segundos" << endl;
+		break;
+	case 7:
+		cout << "e Sete Segundos" << endl;
+		break;
+	case 8:
+		cout << "e Oito Segundos" << endl;
+		break;
+	case 9:
+		cout << "e Nove Segundos" << endl;
+		break;
+	case 10:
+		cout << "e Dez Segundos" << endl;
+		break;
+	case 11:
+		cout << "e Onze Segundos" << endl;
+		break;
+	case 12:
+		cout << "e Doze Segundos" << endl;
+		break;
+	case 13:
+		cout << "e Treze Segundos" << endl;
+		break;
+	case 14:
+		cout << "e Quatorze Segundos" << endl;
+		break;
+	case 15:
+		cout << "e Quinze Segundos" << endl;
+		break;
+	case 16:
+		cout << "e Dezesseis Segundos" << endl;
+		break;
+	case 17:
+		cout << "e Dezessete Segundos" << endl;
+		break;
+	case 18:
+		cout << "e Dezoito Segundos" << endl;
+		break;
+	case 19:
+		cout << "e Dezenove Segundos" << endl;
+		break;
+	case 20:
+		cout << "e Vinte Segundos" << endl;
+		break;
+	case 21:
+		cout << "e Vinte e Um Segundos" << endl;
+		break;
+	case 22:
+		cout << "e Vinte e Dois Segundos" << endl;
+		break;
+	case 23:
+		cout << "e Vinte e Três Segundos" << endl;
+		break;
+	case 24:
+		cout << "e Vinte e Quatro Segundos" << endl;
+		break;
+	case 25:
+		cout << "e Vinte e Cinco Segundos" << endl;
+		break;
+	case 26:
+		cout << "e Vinte e Seis Segundos" << endl;
+		break;
+	case 27:
+		cout << "e Vinte e Sete Segundos" << endl;
+		break;
+	case 28:
+		cout << "e Vinte e Oito Segundos" << endl;
+		break;
+	case 29:
+		cout << "e Vinte e Nove Segundos" << endl;
+		break;
+	case 30:
+		cout << "e Trinta Segundos" << endl;
+		break;
+	case 31:
+		cout << "e Trinta e Um Segundos" << endl;
+		break;
+	case 32:
+		cout << "e Trinta e Dois Segundos" << endl;
+		break;
+	case 33:
+		cout << "e Trinta e Três Segundos" << endl;
+		break;
+	case 34:
+		cout << "e Trinta e Quatro Segundos" << endl;
+		break;
+	case 35:
+		cout << "e Trinta e Cinco Segundos" << endl;
+		break;
+	case 36:
+		cout << "e Trinta e Seis Segundos" << endl;
+		break;
+	case 37:
+		cout << "e Trinta e Sete Segundos" << endl;
+		break;
+	case 38:
+		cout << "e Trinta e Oito Segundos" << endl;
+		break;
+	case 39:
+		cout << "e Trinta e Nove Segundos" << endl;
+		break;
+	case 40:
+		cout << "e Quarenta Segundos" << endl;
+		break;
+	case 41:
+		cout << "e Quarenta e Um Segundos" << endl;
+		break;
+	case 42:
+		cout << "e Quarenta e Dois Segundos" << endl;
+		break;
+	case 43:
+		cout << "e Quarenta e Três Segundos" << endl;
+		break;
+	case 44:
+		cout << "e Quarenta e Quatro Segundos" << endl;
+		break;
+	case 45:
+		cout << "e Quarenta e Cinco Segundos" << endl;
+		break;
+	case 46:
+		cout << "e Quarenta e Seis Segundos" << endl;
+		break;
+	case 47:
+		cout << "e Quarenta e Sete Segundos" << endl;
+		break;
+	case 48:
+		cout << "e Quarenta e Oito Segundos" << endl;
+		break;
+	case 49:
+		cout << "e Quarenta e Nove Segundos" << endl;
+		break;
+	case 50:
+		cout << "e Cinquenta Segundos" << endl;
+		break;
+	case 51:
+		cout << "e Cinquenta e Um Segundos" << endl;
+		break;
+	case 52:
+		cout << "e Cinquenta e Dois Segundos" << endl;
+		break;
+	case 53:
+		cout << "e Cinquenta e Três Segundos" << endl;
+		break;
+	case 54:
+		cout << "e Cinquenta e Quatro Segundos" << endl;
+		break;
+	case 55:
+		cout << "e Cinquenta e Cinco Segundos" << endl;
+		break;
+	case 56:
+		cout << "e Cinquenta e Seis Segundos" << endl;
+		break;
+	case 57:
+		cout << "e Cinquenta e Sete Segundos" << endl;
+		break;
+	case 58:
+		cout << "e Cinquenta e Oito Segundos" << endl;
+		break;
+	case 59:
+		cout << "e Cinquenta e Nove Segundos" << endl;
+		break;
+	case 60:
+		cout << "e Sessenta Segundos";
+		break;
+	}
+
+	system("pause");
+
+}
